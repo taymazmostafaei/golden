77b7424eee4d568bf3b64ffd068fc9fc041a1030
Hossein <81298314+hosseinghafouri@users.noreply.github.com>
2024-02-04 01:22:37 -0800

    finish list user ui

--- config/variables.php
+++ config/variables.php
@@ -3,7 +3,7 @@
 return [
   "creatorName" => "Pixinvent",
   "creatorUrl" => "https://pixinvent.com",
-  "templateName" => "Vuexy",
+  "templateName" => "طلای قالیچی",
   "templateSuffix" => "Bootstrap Admin Template",
   "templateVersion" => "1.0.0",
   "templateFree" => false,
